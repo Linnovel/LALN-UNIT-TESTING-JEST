73101b93bec740c5a6b9543bdb675577dfceb923
Linnovel <luislinnovel@gmail.com>
2023-07-04 04:20:36 +0000

    Unit testinting done I think

---
+++ test.js
@@ -0,0 +1,48 @@
+const { sum } = require('./app.js');
+
+test('adds 14 + 9 to equal 23', () => {
+    let total = sum(14,9);
+    expect(total).toBe(23)
+})
+
+test("One euro should be 1.206 dollars", function(){
+    //import the function from app.js
+    const { fromEuroToDollar } = require('./app.js')
+
+    // use the function like its suppoed to be used
+    const dollars = fromEuroToDollar(3.5)
+
+    // if 1 euro are 1.206 dollars, then 3.5 euros should be (3.5 * 1.2)
+    const expected = 3.5 * 1.2; 
+    
+    // this is the comparison for the unit test
+     expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro are 1.2 dolares, then 3.5 euros should be = (3.5 * 1.2)
+})
+
+
+
+test("One Dollar should be 153.48 Yen", function(){
+
+    const { fromDollarToYen } = require('./app.js')
+
+    const yens = fromDollarToYen(1.2)
+
+ 
+    const expected = 1.2 * 127.9; 
+    
+    
+     expect(fromDollarToYen(1.2)).toBe(153.48); 
+})
+
+test("One yen should be 102.32000000000001 Pound", function(){
+
+    const { fromYenToPound } = require('./app.js')
+
+    const pound = fromYenToPound(0.8)
+
+ 
+    const expected = 127.9 * 0.8;
+    
+    
+     expect(fromYenToPound(127.9)).toBe(102.32000000000001); 
+})
